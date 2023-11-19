19bf527d428f3e01ee4e08f755af5fb0d163c57a
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2023-11-18 23:31:32 -0300

    feat: add login and modal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Modal from "@/components/modal"
 import { getCurrentUser } from "@/lib/session"
 export default async function Home() {
   const user = await getCurrentUser()
   return (
     <div>
+      <Modal/>
       <h1>{JSON.stringify(user)}</h1>
     </div>
   )
